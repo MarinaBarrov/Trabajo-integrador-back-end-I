060a0bec4ffa92463b7f077bdf9a4d4a33ce7c67
MarinaBarrov <maari-b@live.com.ar>
2024-03-03 19:49:31 -0300

    mi primer commit

---
+++ src/main/java/com/example/TrabajointegradorbackendI/TrabajoIntegradorBackEndIApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrabajointegradorbackendI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoIntegradorBackEndIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoIntegradorBackEndIApplication.class, args);
+	}
+
+}
